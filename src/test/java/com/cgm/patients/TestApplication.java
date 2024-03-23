ff900e0d5bac918d4459e3dc65774d6ee68eec7e
dmytro-303 <dmytro.zozulia@embea.com>
2024-03-23 17:26:45 +0100

    Initial commit

---
+++ src/test/java/com/cgm/patients/TestApplication.java
@@ -0,0 +1,12 @@
+package com.cgm.patients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(Application::main).with(TestApplication.class).run(args);
+    }
+}
